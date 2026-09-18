a6950f356f0954e0ee3aa223b5d68a2632aef972
KyzaaDev <dzakyraufnurfhadillah@gmail.com>
2026-09-18 23:43:29 +0700

    feat(routes): add PageController and route for services

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Service;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $services = Service::all();
+        return view('index', compact('services'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 // ROUTE VIEW
 Route::view('/', 'index');
+
+Route::controller(PageController::class)->name('pages.')->group(function () {
+    route::get('/', 'index')->name('home');
+
+});
+
+// ROUTE FOR Order
+Route::prefix('/orders')->controller(OrderController::class)->name('orders.')->group(function () {
+        Route::view('/', 'orders.pesan')->name('create');
+});
